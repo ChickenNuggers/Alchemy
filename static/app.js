d7cd7aeeeba495341cc13630e1e78356780a8bf7
ChickenNuggers <charlesh2016@usd402.com>
2015-11-17 12:42:37 -0600

    app.js, main.css, and starter app

---
+++ static/app.js
@@ -0,0 +1,7 @@
+var app = angular.module('Alchemy', ['ngMaterial', 'ngMdIcons']);
+
+app.config(function($mdThemingProvider) {
+	$mdThemingProvider.theme('default')
+		.primaryPalette('blue')
+		.accentPalette('pink');
+});
