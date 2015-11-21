7af419d5c550bcb9e3751c21190bb4911fe53719
ChickenNuggers <charlesh2016@usd402.com>
2015-11-21 00:03:32 -0600

    blah. rendering the 403 doesn't work. halp.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ except ImportError:
     print("Did you remember to run `pip install -r requirements.txt`?")
     raise SystemExit
 
+with open("config.json") as conffile:
+    config = json.loads(conffile.read())
+
 app = flask.Flask(__name__)
 app.jinja_env.autoescape = False
 
@@ -20,6 +23,25 @@ for module in _module_list:
 enabled_modules = [getattr(modules, module) for module in _module_list if getattr(getattr(modules, module), "render")]
 
 _module_pattern = re.compile(r'modules\.(.+)')
+
+if 'whitelist' in config.keys():
+    _whitelist = []
+    for ip in config['whitelist']:
+        _whitelist.append(re.compile(re.escape(ip)))
+    @app.before_request
+    def whitelist():
+        is_allowed = False
+        for pattern in _whitelist:
+            if pattern.match(flask.request.remote_addr):
+                is_allowed = True
+        if not is_allowed:
+            print('Unallowed user: ' + flask.request.remote_addr)
+            flask.abort(403)
+
+@app.errorhandler(403)
+def access_error():
+    #res = render_template('error.html', {'error': '403', 'message': 'Access Denied', 'reason': 'Blacklisted IP'})
+    return 'no', 403
 
 @app.route("/")
 def master():
@@ -33,6 +55,8 @@ def master():
         elements['module_data'][_module_pattern.match(module.__name__).group(1).capitalize()] = module.render()
     return flask.render_template("index.html", **elements)
 
+_acceptable_settings = ['host', 'port', 'use_reloader']
+
 if __name__ == "__main__":
-    with open("config.json") as config:
-        app.run(**json.loads(config.read()))
+    werkzeug_settings = [(key, value) for key, value in config.items() if key in _acceptable_settings]
+    app.run(**dict(werkzeug_settings))
