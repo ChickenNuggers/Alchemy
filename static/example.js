ceb4d7911ba0fb936cebc595cedcaeb319eb7b8b
Ryan Rion <charlesh2016@usd402.com>
2016-10-26 20:12:21 -0700

    example module: cleaner error handling

--- static/example.js
+++ static/example.js
@@ -1,10 +1,32 @@
-setInterval(function() {
+let interval; // vim:set noet sts=0 sw=4 ts=4:
+var snackbarContainer = document.querySelector("#ex-snackbar")
+
+function make_it_do_thing() {
 	$.getJSON("/example").then( (data)=> {
 		for (key in data) {
 			$("#" + data[key].label_safe).css(
 				'width', data[key].value + "%");
 			$("#content-" + data[key].label_safe).html(
 				data[key].value + "%");
+			if ($("#ex-has-fail").css("display") == "block") {
+				$("#ex-has-fail").css("display", "none");
+			}
 		}
+	}).fail(()=>{
+		clearInterval(interval);
+		$("#ex-has-fail").css("display", "block");
+		snackbarContainer.MaterialSnackbar.showSnackbar({
+			message: "Unable to connect for module: example",
+			timeout: 5000,
+			actionHandler: (()=> window.location.reload()),
+			actionText: "Reload"
+		});
 	});
-}, 5000);
+}
+
+var ex_refresh = ()=> {
+	interval = setInterval(make_it_do_thing, 5000);
+	make_it_do_thing();
+}
+
+ex_refresh();
